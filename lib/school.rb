12339368e7d634c7b3e46df376619a0d7d7e1dbc
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-11-18 13:42:22 -0800

    commit tree 61c5a95d8ca2a00c245d2c585b0d4c39655a9a7b

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+
+class School
+attr_accessor :school_name, :roster
+
+  def initialize(school_name)
+    @roster= Hash.new do |key, value|
+      key[value] = []
+    end 
+  
+  end 
+   def add_student(student_name, grade)
+    @roster[grade] << student_name
+   end 
+   def grade(year)
+    roster[year]
+   end 
+   def sort
+      name_arr = {}
+      roster.each do |grade, student_name|
+        name_arr[grade] = student_name.sort
+      end 
+      name_arr
+    end 
+end 
